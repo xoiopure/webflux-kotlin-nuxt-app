c36574a2297b9dacfe15ab895924d65961332644
Maksim Kostromin <daggerok@gmail.com>
2019-12-20 19:08:34 +0200

    if you're not using et, fuck off (C) whatthecommit.com

---
+++ ui/store/menus.js
@@ -0,0 +1,36 @@
+/**
+ * Private API: State container. Uses directly by mutations.
+ */
+export const state = () => ({
+  title: 'Twimmer',
+  sidebarEnabled: false,
+});
+
+/**
+ * Private API: State mutations. Uses directly only by actions.
+ */
+export const mutations = {
+  setSidebarEnabled(state, value) {
+    state.sidebarEnabled = value;
+  },
+};
+
+/**
+ * Public API: Can be directly used in App and in Actions.
+ */
+export const getters = {
+  getTitle: state => state.title,
+  isSidebarEnabled: state => state.sidebarEnabled,
+};
+
+/**
+ * Public API: Can be directly used in App.
+ */
+export const actions = {
+  toggleSidebar({ commit, getters, state, dispatch }) {
+    commit('setSidebarEnabled', !getters.isSidebarEnabled);
+  },
+  setSidebarEnabled({ commit, getters, state, dispatch }, value) {
+    commit('setSidebarEnabled', value);
+  },
+};
